c43e3d1ae51dbe8000beb5060f3a100f180c875e
Twan <71022124+twan102018@users.noreply.github.com>
2024-02-15 20:44:13 +0100

    magic hand

--- untitled folder/resources/js/main.js
+++ untitled folder/resources/js/main.js
@@ -8,50 +8,82 @@ AOS.init({
   anchorPlacement: 'top-bottom',
 });
 
+const result = document.getElementById("result");
+const form = document.getElementById("form");
+
 console.log("Before fetch request");
-fetch("https://api.web3forms.com/submit", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json"
-  },
-  body: json
-})
-  .then(async (response) => {
-    console.log("Inside fetch .then");
-    let json = await response.json();
-    if (response.status == 200) {
-      console.log("Success:", json);
-      result.innerHTML = json.message;
-      result.classList.remove("text-gray-500");
-      result.classList.add("text-green-500");
-    } else {
-      console.log("Error:", response);
-      result.innerHTML = json.message;
-      result.classList.remove("text-gray-500");
-      result.classList.add("text-red-500");
-    }
-  })
-  .catch((error) => {
-    console.log("Fetch error:", error);
-    result.innerHTML = "Something went wrong!";
-  })
-  .then(function () {
-    console.log("After fetch request");
-    form.reset();
-    setTimeout(() => {
-      result.style.display = "none";
-    }, 5000);
-  });
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const formData = new FormData(form);
+  const json = {};
 
-  function showPasswordPrompt() {
-    var password = prompt("Enter the password:");
-    
-    // simpel wachtwoord
-    if (password === '088901@glr.nl') {
-      window.location.href = 'resources/images/cv.pdf';
-    } else {
-      alert("Incorrect password. Please try again.");
-    }
+  for (const [key, value] of formData.entries()) {
+    json[key] = value;
   }
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(json),
+  })
+    .then(async (response) => {
+      console.log("Inside fetch .then");
+      let jsonResponse = await response.json();
+      if (response.status == 200) {
+        console.log("Success:", jsonResponse);
+        result.innerHTML = jsonResponse.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-green-500");
+      } else {
+        console.log("Error:", response);
+        result.innerHTML = jsonResponse.message;
+        result.classList.remove("text-gray-500");
+        result.classList.add("text-red-500");
+      }
+    })
+    .catch((error) => {
+      console.log("Fetch error:", error);
+      result.innerHTML = "Something went wrong!";
+    })
+    .then(() => {
+      console.log("After fetch request");
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 5000);
+    });
+});
+
+gsap.registerPlugin(MotionPathPlugin);
+
+const tween = gsap.timeline();
+tween.to(".Naam", {
+  duration: 1,
+  ease: "power1.inOut",
+  motionPath: {
+    path: [
+      { x: 500, y: 100 },
+      { x: 750, y: -100 },
+      { x: 350, y: -50 },
+      { x: 600, y: 100 },
+      { x: 800, y: 0 },
+      { x: window.innerWidth, y: -250 },
+    ],
+    curviness: 2,
+    autoRotate: true,
+  },
+});
+
+const controller = new ScrollMagic.Controller();
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".animation",
+  duration: 2000,
+  triggerHook: 0,
+})
+  .setTween(tween)
+  .setPin(".animation")
+  .addTo(controller);
