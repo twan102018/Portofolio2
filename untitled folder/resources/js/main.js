6f3838e2e4041d9a43369b4b1fd7cad48f2fc159
Twan <71022124+twan102018@users.noreply.github.com>
2024-02-21 14:27:18 +0100

    update fix removal magic hand

--- untitled folder/resources/js/main.js
+++ untitled folder/resources/js/main.js
@@ -58,32 +58,3 @@ form.addEventListener("submit", (event) => {
     });
 });
 
-gsap.registerPlugin(MotionPathPlugin);
-
-const tween = gsap.timeline();
-tween.to(".Naam", {
-  duration: 1,
-  ease: "power1.inOut",
-  motionPath: {
-    path: [
-      { x: 500, y: 100 },
-      { x: 750, y: -100 },
-      { x: 350, y: -50 },
-      { x: 600, y: 100 },
-      { x: 800, y: 0 },
-      { x: window.innerWidth, y: -250 },
-    ],
-    curviness: 2,
-    autoRotate: true,
-  },
-});
-
-const controller = new ScrollMagic.Controller();
-const scene = new ScrollMagic.Scene({
-  triggerElement: ".animation",
-  duration: 2000,
-  triggerHook: 0,
-})
-  .setTween(tween)
-  .setPin(".animation")
-  .addTo(controller);
